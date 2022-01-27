1d020d61f05618bb0ad2598e5cd32b25e6547db1
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 21:02:31 +0100

    start of forms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
 const App = (props) => {
-  const { notes } = props
+    // Uses the useState function to initialize the piece of state stored in notes with the array of notes passed in the props
+    const [notes, setNotes]  = useState(props.notes);
+    const [newNote, setNewNote] = useState(
+        'a new note...'
+    )
 
-  return (
+    // Event handler
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notes.length + 1,
+        }
+
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    }
+
+    return (
       <div>
         <h1>Notes</h1>
         <ul>
@@ -12,8 +35,14 @@ const App = (props) => {
                 <Note key={note.id} note={note} />
             )}
         </ul>
+          <form onSubmit={addNote}>
+              <input value={newNote}
+                    onChange={handleNoteChange}
+              />
+              <button type="submit">save</button>
+          </form>
       </div>
-  )
+    )
 }
 
 export default App
