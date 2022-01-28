56292cdc5a5a1e083e21d66751fe329f8bb7e43e
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-28 17:28:19 +0100

    fake db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+
+promise.then(response => {
+    console.log(response)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
 const persons = [
     {
         id: 1,
