bdaacd4bfb57717a54ea6a6f9830b12c7b597f32
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-28 07:51:47 +0100

    firt implem

--- src/App.js
+++ src/App.js
@@ -1,60 +1,46 @@
-import React, {useState} from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react'
 
-const App = (props) => {
-    // Uses the useState function to initialize the piece of state stored in notes with the array of notes passed in the props
-    const [notes, setNotes]  = useState(props.notes);
-    const [newNote, setNewNote] = useState('');
-    const [showAll, setShowAll] = useState(true);
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
 
-    // Event handler
-    const addNote = (event) => {
-        event.preventDefault()
-
-        // Create a new object that represents a Note
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() < 0.5,
-            id: notes.length + 1,
+    const addNewPersons = (event) => {
+        event.preventDefault();
+        const personObject = {
+            name: 'Robin Gh'
         }
 
-        // Use concat to add the new noteObject to existing "notes"
-        // Creates new copy of the array with the new item added to the end
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+        setPersons(persons.concat(personObject));
+        setNewName('');
     }
 
-    const handleNoteChange = (event) => {
-        console.log(event.target.value);
-        setNewNote(event.target.value);
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
     }
-
-    // Filter
-    const notesToShow = showAll
-        ? notes
-        : notes.filter(note => note.important);
 
     return (
-      <div>
-        <h1>Notes</h1>
-          <div>
-              <button onClick={() => setShowAll(!showAll)}>
-                  show {showAll ? 'important' : 'true'}
-              </button>
-          </div>
-        <ul>
-            {notesToShow.map(note =>
-                <Note key={note.id} note={note} />
-            )}
-        </ul>
-          <form onSubmit={addNote}>
-              <input value={newNote}
-                    onChange={handleNoteChange}
-              />
-              <button type="submit">save</button>
-          </form>
-      </div>
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name: <input />
+                </div>
+                <div>
+                    <form onSubmit={addNewPersons}>
+                        <input
+                            value={newName}
+                            onChange={handleNameChange}
+                        />
+                        <button type="submit">add</button>
+                    </form>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            ...
+        </div>
     )
 }
 
