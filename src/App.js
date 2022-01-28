1b648618f962f60af92a0051c312895454eebde3
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-28 07:19:37 +0100

    end of materials

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Note from './components/Note'
 const App = (props) => {
     // Uses the useState function to initialize the piece of state stored in notes with the array of notes passed in the props
     const [notes, setNotes]  = useState(props.notes);
-    const [newNote, setNewNote] = useState(
-        'a new note...'
-    )
+    const [newNote, setNewNote] = useState('');
+    const [showAll, setShowAll] = useState(true);
 
     // Event handler
     const addNote = (event) => {
         event.preventDefault()
+
+        // Create a new object that represents a Note
         const noteObject = {
             content: newNote,
             date: new Date().toISOString(),
@@ -18,6 +19,8 @@ const App = (props) => {
             id: notes.length + 1,
         }
 
+        // Use concat to add the new noteObject to existing "notes"
+        // Creates new copy of the array with the new item added to the end
         setNotes(notes.concat(noteObject))
         setNewNote('')
     }
@@ -27,11 +30,21 @@ const App = (props) => {
         setNewNote(event.target.value);
     }
 
+    // Filter
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important);
+
     return (
       <div>
         <h1>Notes</h1>
+          <div>
+              <button onClick={() => setShowAll(!showAll)}>
+                  show {showAll ? 'important' : 'true'}
+              </button>
+          </div>
         <ul>
-            {notes.map(note =>
+            {notesToShow.map(note =>
                 <Note key={note.id} note={note} />
             )}
         </ul>
