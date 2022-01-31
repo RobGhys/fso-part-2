3a4b5230a88f64659d31c7768180acd82597f878
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-31 15:52:25 +0100

    initial backend stuff

--- src/App.js
+++ src/App.js
@@ -1,67 +1,68 @@
-import React, { useState } from 'react'
-import Person from './components/Person'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-const App = (props) => {
-    const [persons, setPersons] = useState(props.persons);
-    const [newName, setNewName] = useState('');
-    const [newTel, setNewTel] = useState('');
+const App = () => {
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(false)
 
-    const addNewPersons = (event) => {
-        event.preventDefault();
+    /**
+     * Function ("effects") gets executed after rendering
+     */
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => { // registers an event handler
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }, []) // how often the effect is run ([] = once)
+    console.log('render', notes.length, 'notes')
 
-        const personObject = {
-            id: persons.length + 1,
-            name: newName,
-            tel: newTel
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
         }
 
-        let nameExists = false;
-
-        persons.forEach(persons => {
-            if (persons.name === personObject.name) {
-                nameExists = true;
-            }
-        })
-
-        if (nameExists === false) {
-            setPersons(persons.concat(personObject));
-            setNewName('');
-            setNewTel('');
-        } else {
-            alert(`${personObject.name} is already present in the phone book`)
-        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
     }
 
-    const handleNameChange = (event) => {
+    const handleNoteChange = (event) => {
         console.log(event.target.value)
-        setNewName(event.target.value)
+        setNewNote(event.target.value)
     }
 
-    const handleTelChange = (event) => {
-        console.log(event.target.value)
-        setNewTel(event.target.value)
-    }
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
 
     return (
         <div>
-            <h2>Phonebook</h2>
-            <form onSubmit={addNewPersons}>
-               <div>name : <input
-                   value={newName}
-                   onChange={handleNameChange}
-               /></div>
-                <div>Tel : <input
-                    value={newTel}
-                    onChange={handleTelChange}
-                /></div>
-                <button type="submit">add</button>
-            </form>
-            <h2>Numbers</h2>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all' }
+                </button>
+            </div>
             <ul>
-                {persons.map(person =>
-                <Person key={person.id} person={person} />
+                {notesToShow.map(note =>
+                    <Note key={note.id} note={note} />
                 )}
             </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import axios from 'axios'
-
-const promise = axios.get('http://localhost:3001/notes')
-
-promise.then(response => {
-    console.log(response)
-})
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-const persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        tel: '026522171'
-    }
-]
-
-ReactDOM.render(
-    <App persons={persons} />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
